1b43180bb24ceb30d2fdd6feec3249f08f5624df
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-06-29 18:29:20 -0500

    started on the assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,15 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+const basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    return true
+}
+addItem('apples')
+console.log(addItem('peaches'), basket)
+
+function listItems(){
+    
+}
